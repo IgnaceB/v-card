d5c7d3c0a01896a58a0e716503fc9f70a62eb4a0
IgnaceB <ignacebernard@hotmail.com>
2023-05-19 10:01:27 +0200

    test branch

--- script.js
+++
@@ -1,9 +0,0 @@
-
-
-document.getElementById('test').style.display = "none";
-
-function openTest() {
-    document.getElementById('test').style.display="block";
-}
-;
-document.getElementById('BTN').addEventListener('click',openTest());
